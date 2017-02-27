ee2ad90e403e440c5911066438d0da3bd3892ca4
Duc Nguyen <nhduc.lhp@gmail.com>
2017-02-27 11:22:18 +0800

    Remove unused imports in Exploration Solver

--- src/mdp/solver/exploration/ExplorationSolver.java
+++ src/mdp/solver/exploration/ExplorationSolver.java
@@ -1,21 +1,10 @@
 package mdp.solver.exploration;
-import java.util.ArrayList;
 
-
-
-import java.util.List;
-
-import com.apple.eawt.AppEvent.SystemSleepEvent;
-
-import mdp.map.Waypoint;
 import mdp.common.Direction;
 import mdp.map.Map;
 import mdp.robot.Robot;
 import mdp.common.Vector2;
-import mdp.map.WPObstacleState;
 import mdp.solver.exploration.MapViewer;
-import mdp.robot.RobotAction;
-import java.util.LinkedList;
 
 public class ExplorationSolver {
     private static Map objective_map = new Map(); // generated from map solver
